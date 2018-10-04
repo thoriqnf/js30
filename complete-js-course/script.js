273b2868003e7cad2e9fc050f6aa5fcbff50029c
thoriq <thoriqnfaizal@gmail.com>
2018-10-04 23:02:12 +0700

    Type Coercion and Variable Mutation

---
+++ complete-js-course/script.js
@@ -0,0 +1,46 @@
+/*
+  Variables and data types
+*/
+
+// var fisrtName = 'John';
+// console.log(fisrtName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+// var 3years = 3;
+
+/*
+  Variable mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+
+//Type coercion
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + 'Is he married? ' + isMarried);
+
+//Variable Mutation
+age = 'twenty nine';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old' + job + '. Is he married? ' + isMarried);
+
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
