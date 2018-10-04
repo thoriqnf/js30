167ae241845a06f0d744a2bd0f579bf5bf9d844d
thoriq <thoriqnfaizal@gmail.com>
2018-10-04 22:40:31 +0700

    Variable & data types

---
+++ script.js
@@ -0,0 +1,17 @@
+var fisrtName = 'John';
+console.log(fisrtName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var 3years = 3;
