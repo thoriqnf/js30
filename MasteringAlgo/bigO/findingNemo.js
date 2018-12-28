e10801f1b1ed7b44c2374ce4522b89ce0dad2920
thoriq <thoriqnfaizal@gmail.com>
2018-12-28 14:40:50 +0700

    Finishing last commit

--- MasteringAlgo/bigO/findingNemo.js
+++ MasteringAlgo/bigO/findingNemo.js
@@ -1,14 +1,14 @@
-const nemo = ['nema'];
+const nemo = ["nema"];
 
 function findNemo(array) {
   var t0 = performance.now();
-  for (let i = 0; i < array.length; i++){
-    if (array[i] === 'nema'){ 
-      console.log('Found Nemo');
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nema") {
+      console.log("Found Nemo");
     }
   }
   var t1 = performance.now();
-  console.log('call to find Nemo took' + (t1-t0) + ' milliseconds');
+  console.log("call to find Nemo took" + (t1 - t0) + " milliseconds");
 }
 
 findNemo(nemo);
