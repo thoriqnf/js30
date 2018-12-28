feaf33e51c01da2039c8aa5d291bc55cc3b3c5ba
thoriq <thoriqnfaizal@gmail.com>
2018-12-29 01:10:02 +0700

    Adding babel-loader

--- react-boilerplate/webpack.config.js
+++ react-boilerplate/webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
   }
 }
 //mode development yang normal ada spacing bisa di baca
