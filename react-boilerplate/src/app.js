3eb40444ac3cec047cb7eacc573972c518ab9dcd
thoriq <thoriqnfaizal@gmail.com>
2018-12-29 13:24:00 +0700

    Adding source map for debugging

--- react-boilerplate/src/app.js
+++ react-boilerplate/src/app.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World Haniku sayang cinta</h1>
-  }
+    return <h1>Hello World</h1>
+  };
 };
 
 export default App;
