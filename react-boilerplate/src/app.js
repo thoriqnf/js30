01b292befda7907bfec933c1ba734805dd0fd7d5
thoriq <thoriqnfaizal@gmail.com>
2018-12-29 14:02:25 +0700

    Adding hot reloader

--- react-boilerplate/src/app.js
+++ react-boilerplate/src/app.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import { hot } from 'react-hot-loader';
 class App extends React.Component {
   state = {
     count: 0
   }
 
   render() {
+    const { count } = this.state
     return (
       <div>
-        <h1>Hello World</h1>
-        <h2>Count: {this.state.count}</h2>
+        <h1>Hello World!!!!!!</h1>
+        <h2 className={count > 10 ? 'warning' : null}>
+          Count: {count}
+        </h2>
         <button onClick={() => this.setState(state => ({ count: state.count + 1 }))}>+</button>
         <button onClick={() => this.setState(state => ({ count: state.count - 1 }))}>-</button>
       </div>
@@ -17,4 +20,6 @@ class App extends React.Component {
   };
 };
 
-export default App;
+export default hot(module)(App);
+
+//hot reloader can reload without dump the state
